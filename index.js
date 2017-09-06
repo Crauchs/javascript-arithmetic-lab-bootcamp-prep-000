339c815a9bd08cbbb4f87bb76de2589e841295d5
Kenneth Cravens <bombfodder93@gmail.com>
2017-09-06 02:59:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,4 +4,5 @@ function divide(a, b) {return a/b}
 function dec(n) {return --n}
 function subtract(a, b) {return a-b}
 function inc(n) {return ++n}
-function makeInt(n) {return parseInt(n)}
+function makeInt(n) {return parseInt(n, 10)}
+function preserveDecimal(n) {return parseFloat(n)}
