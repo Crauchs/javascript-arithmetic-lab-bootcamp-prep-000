f95083e5121dedaaddc563169e0f101deaaad016
Kenneth Cravens <bombfodder93@gmail.com>
2017-09-06 02:58:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,6 @@ function add(a, b) {return a+b}
 function multiply(a, b) {return a*b}
 function divide(a, b) {return a/b}
 function dec(n) {return --n}
+function subtract(a, b) {return a-b}
+function inc(n) {return ++n}
+function makeInt(n) {return parseInt(n)}
